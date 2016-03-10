7b146d9558a50785a5e8fff0610a60530431e6c4
Baptiste JAMIN <bjamin@e2r2p19.42.fr>
2016-03-10 16:25:13 +0100

    First com.m.it

---
+++ linter.py
@@ -0,0 +1,36 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Baptiste JAMIN
+# Copyright (c) 2016 Baptiste JAMIN
+#
+# License: MIT
+#
+
+"""This module exports the 42Norminette plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class 42Norminette(Linter):
+    """Provides an interface to norminette."""
+
+    syntax = 'c'
+    executable = 'norminette'
+
+    regex = (
+        r'^(?:(?P<error>Error)|(?P<warning>Warning))'
+        r'.+?line (?P<line>\d+)+?'
+        r'(?P<message>.+)'
+    )
+
+    multiline = True
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    defaults = {}
+
+    def cmd(self):
+        result = self.executable
+        return result + ' ' + sublime.active_window().active_view().file_name()
+
